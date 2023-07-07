3af7c055dc57b7e99b964d134708f04807d2040c
cuisongliu <cuisongliu@qq.com>
2023-07-07 17:20:25 +0800

    add plugins for kubebuilder

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/labring/kubebuilder-helm
+
+go 1.20
+
+require sigs.k8s.io/kubebuilder/v3 v3.11.1
+
+require (
+	github.com/gobuffalo/flect v1.0.2 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/afero v1.9.5 // indirect
+	github.com/spf13/cobra v1.7.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/mod v0.11.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
+	golang.org/x/tools v0.10.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,59 @@
+/*
+Copyright 2023 cuisongliu@qq.com.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"github.com/labring/kubebuilder-helm/plugins"
+	golangv4 "github.com/labring/kubebuilder-helm/plugins/golang/v4"
+	helmv1 "github.com/labring/kubebuilder-helm/plugins/helm/v1"
+	"log"
+	"sigs.k8s.io/kubebuilder/v3/pkg/cli"
+	cfgv3 "sigs.k8s.io/kubebuilder/v3/pkg/config/v3"
+	"sigs.k8s.io/kubebuilder/v3/pkg/plugin"
+)
+
+func main() {
+	gov4Bundle, _ := plugin.NewBundleWithOptions(plugin.WithName(plugins.DefaultNameQualifier),
+		plugin.WithVersion(plugin.Version{Number: 4}),
+		plugin.WithPlugins(helmv1.Plugin{}, golangv4.Plugin{}),
+	)
+	c, err := cli.New(
+		cli.WithCommandName("kubebuilder-helm"),
+		cli.WithVersion(versionString()),
+		cli.WithExtraCommands(), // 如果有额外的命令
+		cli.WithPlugins(
+			gov4Bundle,
+			// 可以添加其他插件
+		),
+		cli.WithDefaultPlugins(cfgv3.Version, gov4Bundle),
+		cli.WithCompletion(),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := c.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// versionString returns the CLI version
+func versionString() string {
+	// return your binary project version
+	return "v0.0.1"
+}
